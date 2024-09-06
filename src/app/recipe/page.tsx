487db80d1df52ce51a67da67bfd5a51d63d7403f
hatbann <0urloveis_great@naver.com>
2024-09-06 10:56:59 +0900

    (feat) Add middleware to route

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -1,29 +1,29 @@
 /** @format */
 
 /* "use client"; */
-"use server";
+'use server';
 
-import { RecipeType } from "@/model/recipe";
-import React from "react";
-import RecipeView from "@/components/recipe/view";
-import { revalidatePath } from "next/cache";
+import { RecipeType } from '@/model/recipe';
+import React from 'react';
+import RecipeView from '@/components/recipe/view';
+import { revalidatePath } from 'next/cache';
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
 const getInitialPageData = async ({ searchParams }: Props) => {
-  const page = searchParams.page ?? "1";
+  const page = searchParams.page ?? '1';
   try {
     const API_URL =
-      process.env.NODE_ENV === "production"
-        ? "/api"
+      process.env.NODE_ENV === 'production'
+        ? '/api'
         : `${process.env.NEXT_PUBLIC_API_URL!}/api`;
 
     const response = await fetch(`${API_URL}/recipe?page=${page}`, {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      method: "GET",
+      method: 'GET',
     })
       .then((res) => {
         return res.json();
@@ -31,44 +31,52 @@ const getInitialPageData = async ({ searchParams }: Props) => {
       .catch((e) => {
         console.log(e);
       });
-    const recipes = response.recipes;
-    const totalCount = response.total;
-    if (recipes) {
-      const users = recipes.map((res: RecipeType) => {
-        if (res.author) return res.author;
-      });
-      if (users.length !== 0) {
-        const id = String(users);
-        const userData = await fetch(`${API_URL}/user/nickname/${id}`, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          method: "GET",
-        }).then((res) => res.json());
-        const userArr: { id: string; nickname: string }[] = userData.data;
-        const recipeRes: RecipeType[] = [];
-        recipes.map((item: RecipeType) => {
-          const name = userArr.find(
-            (data) => data.id === item.author
-          )?.nickname;
-          const temp: RecipeType = {
-            _id: item._id,
-            title: item.title,
-            description: item.description,
-            img: item.img,
-            views: item.views,
-            steps: item.steps,
-            ingredients: item.ingredients,
-            author: name,
-            created_at: item.created_at,
-            updated_at: item.updated_at,
-          };
-          recipeRes.push(temp);
+
+    if (response) {
+      const recipes = response.recipes;
+      const totalCount = response.total;
+      if (recipes) {
+        const users = recipes.map((res: RecipeType) => {
+          if (res.author) return res.author;
         });
-        return {
-          recipeRes,
-          totalCount,
-        };
+        if (users.length !== 0) {
+          const id = String(users);
+          const userData = await fetch(`${API_URL}/user/nickname/${id}`, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'GET',
+          }).then((res) => res.json());
+          const userArr: { id: string; nickname: string }[] = userData.data;
+          const recipeRes: RecipeType[] = [];
+          recipes.map((item: RecipeType) => {
+            const name = userArr.find(
+              (data) => data.id === item.author
+            )?.nickname;
+            const temp: RecipeType = {
+              _id: item._id,
+              title: item.title,
+              description: item.description,
+              img: item.img,
+              views: item.views,
+              steps: item.steps,
+              ingredients: item.ingredients,
+              author: name,
+              created_at: item.created_at,
+              updated_at: item.updated_at,
+            };
+            recipeRes.push(temp);
+          });
+          return {
+            recipeRes,
+            totalCount,
+          };
+        } else {
+          return {
+            recipeRes: [],
+            totalCount: 0,
+          };
+        }
       } else {
         return {
           recipeRes: [],
@@ -88,8 +96,8 @@ const getInitialPageData = async ({ searchParams }: Props) => {
 
 export default async function ({ searchParams }: Props) {
   async function revalidate() {
-    "use server";
-    revalidatePath("/recipe");
+    'use server';
+    revalidatePath('/recipe');
   }
 
   revalidate();
